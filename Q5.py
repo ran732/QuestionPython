5ef3f2395554c1c28043546ec516c172f63c0021
ran702 <ajeetranjeet495@gmail.com>
2025-12-25 03:21:04 +0530

    character is vowel or not

--- Q5.py
+++ Q5.py
@@ -1,13 +1,13 @@
-# Write a program to find max of two numbers
+# Write a program to find max of three numbers
 
 n1 = eval(input("Enter num1 : "))
 n2 = eval(input("Enter num2 : "))
 n3 = eval(input("Enter num3 : "))
 
-if (n1>=n2 & n1>=n3) :
+if (n1>=n2 and n1>=n3) :
     print(n1," is largest")
     
-elif (n2>=n1 & n2>=n3)  :
+elif (n2>=n1 and n2>=n3)  :
     print(n2," is largest")
     
 else :
---
+++ Q6.py
@@ -0,0 +1,7 @@
+# Write a program to find input character is vowel or not
+
+ch = input ("Enter a chaecter :")
+if (ch=="a" or ch=="A" or  ch=="e" or ch=="E" or ch=="i" or ch=="I" or ch=="O" or ch=="o" or ch=="u" or ch=="U"):
+    print ("Vowel")
+else:
+    print ("Consonant")    
