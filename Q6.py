090d315c7892e4964c145edfde1f653bf92eb048
ran702 <ajeetranjeet495@gmail.com>
2025-12-25 03:37:52 +0530

    input character is vowel or not

--- Q6.py
+++ Q6.py
@@ -1,7 +1,15 @@
 # Write a program to find input character is vowel or not
 
 ch = input ("Enter a chaecter :")
-if (ch=="a" or ch=="A" or  ch=="e" or ch=="E" or ch=="i" or ch=="I" or ch=="O" or ch=="o" or ch=="u" or ch=="U"):
-    print ("Vowel")
+
+# if (ch=="a" or ch=="A" or  ch=="e" or ch=="E" or ch=="i" or ch=="I" or ch=="O" or ch=="o" or ch=="u" or ch=="U"):
+#     print ("Vowel")
+# else:
+#     print ("Consonant")   
+
+
+ 
+if (ch in "AEIOUaeiou "):
+    print("Vowel") 
 else:
-    print ("Consonant")    
+    print("Consonant")    
