5f79976310248e83923e9c4daa557dcf9966b801
ran702 <ajeetranjeet495@gmail.com>
2025-12-25 15:25:15 +0530

    minimum value of input two values

---
+++ Q11.py
@@ -0,0 +1,11 @@
+# Write a program to find minimum value of input two values
+
+num1 = int(input("Enter  num1 "))
+num2 = int(input("Enter  num2 "))
+
+if num1>num2 :
+    print(num1,"is greater number")
+elif num1==num2 :
+    print("num1 = num2 = ",num1)    
+else:    
+    print(num2,"is  greater number")
