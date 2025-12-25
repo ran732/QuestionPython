ef92ed04dd33168f47d9331ae3967f48f094ee86
ran702 <ajeetranjeet495@gmail.com>
2025-12-25 14:54:27 +0530

    divisible with 7 or not

---
+++ Q7.py
@@ -0,0 +1,7 @@
+# Write a program to verify input number divisible with 7 or not
+
+num = int(input("Enter a number : "))
+if num%7==0 :
+    print(" Divisible by 7")
+else :    
+    print("Not Divisible by 7")
