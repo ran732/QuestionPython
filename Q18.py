d2b9de1a34d4c2745c73439421b5099b8a793f57
ran702 <ajeetranjeet495@gmail.com>
2025-12-26 19:06:54 +0530

    length of number or  count digits  number

--- Q18.py
+++ Q18.py
@@ -1,1 +1,14 @@
 # Write a program to find length of number or count digits of input number
+
+
+user=int(input("Enter any number : "))
+count=0
+
+if user==0:
+    print("Length of number is 1")  
+else:
+    while user>0:
+      user =user//10
+      count= count+1
+    
+    print("Length of number is ",count)    
