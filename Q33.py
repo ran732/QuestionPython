64ae12e2840618a84cd8874d18b1d225d2f415ed
ran702 <ajeetranjeet495@gmail.com>
2025-12-28 01:46:35 +0530

    factorial and sum of input number

---
+++ Q33.py
@@ -0,0 +1,11 @@
+# Write a program to find factorial and sum of input number
+
+
+sum=0
+fact=1
+for i in range (1,6):
+    print(i)
+    sum=sum+i
+    fact=fact*i
+print("Sum=",sum)
+print("Factorial=",fact)    
