8f12800cc7832ecaa4361bfb2a1d5540df876bb5
ran702 <ajeetranjeet495@gmail.com>
2026-04-19 12:56:00 +0530

    Constructor

--- OOPS/Constructor/c3.py
+++ OOPS/Constructor/c3.py
@@ -9,12 +9,14 @@ class Calculator:
     
 class Triangle:
     def __init__(self,b=0,h=0):
+        if b<0 or h<0:
+            raise ValueError(" Base and height must be positive.")
         self.base=b
         self.height=h
     def findArea(self):
         a=0.5*self.base*self.height
-        print(f'Area of triangle is {a:2f}.')       
-        return a     
+        print(f'Area of triangle is {a:.2f}.')       
+             
         
 cal=Calculator()
 ad=cal.sum()
@@ -22,5 +24,5 @@ print(ad)
 sb=cal.sub()  
 print(sb)     
 
-t=Triangle(12,4)
-print(t.findArea())
+t=Triangle(12)
+t.findArea()
